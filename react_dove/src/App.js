6c977d0c3862b1d2eb08d82099dfedd66012c8c0
Leslie Castelan <lesliecastelan@Leslies-MacBook-Pro.local>
2021-11-10 20:31:29 -0800

    built login/register

--- react_dove/src/App.js
+++ react_dove/src/App.js
@@ -1,7 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';  Leslie commented this out 
 import './App.css';
 
+
+class App extends React.Component	{
+	render() {
+		return (
+			<div className="App">
+				<div className = "Registration">
+					<h1>Register</h1>
+					<label>Username</label>
+					<input id= "userRegister" type = "text" />
+					<br></br>
+					<br></br>
+					<label>Email Address:</label>
+					<input id= "emailRegister" type="text" />
+					<br></br>
+					<br></br>
+					<label>Password</label>
+					<input id= "passwrdRegister" type="password" />
+					<br></br>
+					<br></br>
+					<button> Register</button>
+					
+				</div>
+				<div className = "Login">
+					<h2>Login</h2>
+
+					<label>Username</label>
+					<input id = "userLogin" type = "text"/>
+					<br></br><br></br>
+
+					<label>Password</label>
+					<input id = "passLogin" type="password" />
+					<br></br>
+					<br></br>
+					<button>Forgot Password?</button>
+
+
+				</div>
+				
+			</div>
+		);
+	}
+}
+
+
+/**				Leslie commented this out 
 function App() {
 	const [data, setData] = React.useState(null);
 
@@ -20,5 +65,5 @@ function App() {
 		</div>
 	);
 }
-
+ */
 export default App;
