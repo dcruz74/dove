a21688b62c7b5b88143c0313e9da98c422f95f0c
Leslie Castelan Chavez <lesliecastelanchavez@gmail.com>
2021-11-27 14:22:07 -0800

    login almost ready for backend

--- react_dove/src/App.js
+++ react_dove/src/App.js
@@ -4,49 +4,85 @@ import './App.css';
 
 
 class App extends React.Component	{
+	constructor() {
+		super() 
+		this.state = {
+			username: '',
+			password: '', 
+			forgotPassword: false,
+		} 
+
+		this.changeUserName = this.changeUserName.bind(this)
+		this.changePassword = this.changePassword.bind(this)
+	}
+
+
+	changeUserName(event) {
+		this.setState({
+			username: event.target.value
+		})
+	}
+
+	changePassword(event) {
+		this.setState({
+			password: event.target.value
+		})
+	}
+
+	onSubmit(event) {
+		event.preventDefault() //prevents form from refreshing
+		const registered = {
+			username: this.state.username,
+			password: this.state.password, 
+		}
+
+
+
+		//SEND THIS REGISTERED TO BACKEND HERE
+
+		//SEND USER TO HOMEPAGE ONCE LOGGED IN
+		//maybe enable some token? hit up ryan 
+		//window.location = '/'
+
+
+	}
+
+
 	render() {
 		return (
-			<div className="App">
-				<div className = "Registration">
-					<h1>Register</h1>
-					<label>Username</label>
-					<input id= "userRegister" type = "text" />
-					<br></br>
-					<br></br>
-					<label>Email Address:</label>
-					<input id= "emailRegister" type="text" />
-					<br></br>
-					<br></br>
-					<label>Password</label>
-					<input id= "passwrdRegister" type="password" />
-					<br></br>
-					<br></br>
-					<button> Register</button>
-					
-				</div>
-				<div className = "Login">
+			<div>
+				<form onSubmit = {this.onSubmit}>
+				<div className = "Login_container">
 					<h2>Login</h2>
 
 					<label>Username</label>
-					<input id = "userLogin" type = "text"/>
+					<input id = "userLogin" type = "text"
+					placeholder= 'Username'
+					onChange= {this.changeUserName}
+					value = {this.state.changeUserName}/>
 					<br></br><br></br>
 
 					<label>Password</label>
-					<input id = "passLogin" type="password" />
-					<br></br>
-					<br></br>
+					<input id = "passLogin" type="password" 
+					placeholder= 'Password'
+					onChange = {this.changePassword}
+					value = {this.state.changePassword}/>
+					<br></br><br></br>
 					<button>Forgot Password?</button>
-
+					
+					<br></br><br></br>
+					<input type= 'submit' value= 'Submit'/>
 
 				</div>
-				
+				</form> 
+		
 			</div>
 		);
 	}
 }
 
 
-/**				Leslie commented this out 
+/**			 
 function App() {
 	const [data, setData] = React.useState(null);
 
@@ -65,5 +101,26 @@ function App() {
 		</div>
 	);
 }
+
+
+
+
+<div className = "Registration">
+					<h1>Register</h1>
+					<label>Username</label>
+					<input id= "userRegister" type = "text" />
+					<br></br>
+					<br></br>
+					<label>Email Address:</label>
+					<input id= "emailRegister" type="text" />
+					<br></br>
+					<br></br>
+					<label>Password</label>
+					<input id= "passwrdRegister" type="password" />
+					<br></br>
+					<br></br>
+					<button> Register</button>
+					
+				</div>
  */
 export default App;
