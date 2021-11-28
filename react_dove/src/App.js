7e66bfca88641c71b77ddff3f4f457960e3e4fe1
Robert Snipes <robertsnipes7@gmail.com>
2021-11-27 17:12:20 -0800

    Integrated design with current repo

--- react_dove/src/App.js
+++ react_dove/src/App.js
@@ -2,90 +2,37 @@ import React from 'react';
 //import logo from './logo.svg';  Leslie commented this out 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
+import Login from './Login';
 import ProfileCards from './ProfileCards';
-import SwipeButtons from './SwipeButtons';
 import './App.css';
 
 
-class App extends React.Component	{
-	constructor() {
-		super() 
-		this.state = {
-			username: '',
-			password: '', 
-			forgotPassword: false,
-		} 
+function App() {
+	return (
+	  <div className="App">
+		{/* Header */}
+		<Header />
+		<Router>
+		  <Routes>
+		  	<Route path="/login" element={<Login/>}>
+			</Route>
+			<Route path="/" element={<ProfileCards/>}>
+			</Route>
+			<Route path="/messenger">
+			</Route>
 
-		this.changeUserName = this.changeUserName.bind(this)
-		this.changePassword = this.changePassword.bind(this)
-		this.onSubmit = this.onSubmit.bind(this)
-
-	}
-
-
-	changeUserName(event) {
-		this.setState({
-			username: event.target.value
-		})
-	}
-
-	changePassword(event) {
-		this.setState({
-			password: event.target.value
-		})
-	}
-
-	onSubmit(event) {
-		event.preventDefault() //prevents form from refreshing
-		const registered = {
-			username: this.state.username,
-			password: this.state.password, 
-		}
-
-
-
-		//SEND THIS REGISTERED TO BACKEND HERE
-
-		//SEND USER TO HOMEPAGE ONCE LOGGED IN
-		//maybe enable some token? hit up ryan 
-		//window.location = '/'
-
-
-	}
-
-
-	render() {
-		return (
-			<div>
-				<form onSubmit = {this.onSubmit}>
-				<div className = "Login_container">
-					<h2>Login</h2>
-
-					<label>Username</label>
-					<input id = "userLogin" type = "text"
-					placeholder= 'Username'
-					onChange= {this.changeUserName}
-					value = {this.state.changeUserName}/>
-					<br></br><br></br>
-
-					<label>Password</label>
-					<input id = "passLogin" type="password" 
-					placeholder= 'Password'
-					onChange = {this.changePassword}
-					value = {this.state.changePassword}/>
-					<br></br><br></br>
-					<button>Forgot Password?</button>
-					
-					<br></br><br></br>
-					<input type= 'submit' value= 'Submit'/>
-
-				</div>
-				</form> 
-		
-			</div>
-		);
-	}
-}
+		  </Routes>
+  
+		  {/* Profile Cards */}
+		  {/* Buttons below profile cards */}
+  
+		  {/* messenger home */}
+		  {/* personl chat */}
+  
+		</Router>
+	  </div>
+	);
+  }
 
 
 /**			 
---
+++ react_dove/src/Login.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+class Login extends React.Component	{
+	constructor() {
+		super() 
+		this.state = {
+			username: '',
+			password: '', 
+			forgotPassword: false,
+		} 
+
+		this.changeUserName = this.changeUserName.bind(this)
+		this.changePassword = this.changePassword.bind(this)
+		this.onSubmit = this.onSubmit.bind(this)
+
+	}
+
+
+	changeUserName(event) {
+		this.setState({
+			username: event.target.value
+		})
+	}
+
+	changePassword(event) {
+		this.setState({
+			password: event.target.value
+		})
+	}
+
+	onSubmit(event) {
+		event.preventDefault() //prevents form from refreshing
+		const registered = {
+			username: this.state.username,
+			password: this.state.password, 
+		}
+
+
+
+		//SEND THIS REGISTERED TO BACKEND HERE
+
+		//SEND USER TO HOMEPAGE ONCE LOGGED IN
+		//maybe enable some token? hit up ryan 
+		//window.location = '/'
+
+
+	}
+
+
+	render() {
+		return (
+			<div>
+				<form onSubmit = {this.onSubmit}>
+				<div className = "Login_container">
+					<h2>Login</h2>
+
+					<label>Username</label>
+					<input id = "userLogin" type = "text"
+					placeholder= 'Username'
+					onChange= {this.changeUserName}
+					value = {this.state.changeUserName}/>
+					<br></br><br></br>
+
+					<label>Password</label>
+					<input id = "passLogin" type="password" 
+					placeholder= 'Password'
+					onChange = {this.changePassword}
+					value = {this.state.changePassword}/>
+					<br></br><br></br>
+					<button>Forgot Password?</button>
+					
+					<br></br><br></br>
+					<input type= 'submit' value= 'Submit'/>
+
+				</div>
+				</form> 
+		
+			</div>
+		);
+	}
+}
+
+export default Login
