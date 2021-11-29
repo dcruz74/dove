bccd9d3be077e55d904385ee30b26ba7b5f2a14b
ryanldoan <69395683+ryanldoan@users.noreply.github.com>
2021-11-28 18:33:18 -0800

    Login works now
    Still need to redirect a successful login after authentication

--- react_dove/src/Login.js
+++ react_dove/src/Login.js
@@ -9,65 +9,60 @@ class Login extends React.Component	{
 			forgotPassword: false,
 		} 
 
-		this.changeUserName = this.changeUserName.bind(this)
-		this.changePassword = this.changePassword.bind(this)
-		this.onSubmit = this.onSubmit.bind(this)
+		// this.changeUserName = this.changeUserName.bind(this)
+		// this.changePassword = this.changePassword.bind(this)
+		// this.onSubmit = this.onSubmit.bind(this)
 
 	}
 
 
-	changeUserName(event) {
-		this.setState({
-			username: event.target.value
-		})
-	}
+	// changeUserName(event) {
+	// 	this.setState({
+	// 		username: event.target.value
+	// 	})
+	// }
 
-	changePassword(event) {
-		this.setState({
-			password: event.target.value
-		})
-	}
+	// changePassword(event) {
+	// 	this.setState({
+	// 		password: event.target.value
+	// 	})
+	// }
 
-	onSubmit(event) {
-		event.preventDefault() //prevents form from refreshing
-		const registered = {
-			username: this.state.username,
-			password: this.state.password, 
-		}
-
-
-
-		//SEND THIS REGISTERED TO BACKEND HERE
-
-		//SEND USER TO HOMEPAGE ONCE LOGGED IN
-		//maybe enable some token? hit up ryan 
-		//window.location = '/'
+	// onSubmit(event) {
+	// 	console.log("hi");
+	// 	event.preventDefault() //prevents form from refreshing
+	// 	const registered = {
+	// 		username: this.state.username,
+	// 		password: this.state.password, 
+	// 	}
 
 
-	}
+
+	// 	//SEND THIS REGISTERED TO BACKEND HERE
+
+	// 	//SEND USER TO HOMEPAGE ONCE LOGGED IN
+	// 	//maybe enable some token? hit up ryan 
+	// 	//window.location = '/'
+
+
+	// }
 
 	render() {
 		return (
 			<div>
-				<form onSubmit = {this.onSubmit}>
+				<form method = "POST" action = "/login">
 				<div className = "Login_container">
 					<h2>Login</h2>
-
+					
 					<label>Username</label>
-					<input id = "userLogin" type = "text"
-					placeholder= 'Username'
-					onChange= {this.changeUserName}
-					value = {this.state.changeUserName}/>
+					<input type = "text" name = "username" placeholder = "username"
+					/>
 					<br></br><br></br>
 
 					<label>Password</label>
-					<input id = "passLogin" type="password" 
-					placeholder= 'Password'
-					onChange = {this.changePassword}
-					value = {this.state.changePassword}/>
-					<br></br><br></br>
-					<button>Forgot Password?</button>
-					
+					<input type = "password" name="password" 
+					placeholder= 'password'
+					/>
 					<br></br><br></br>
 					<input type= 'submit' value= 'Submit'/>
 
