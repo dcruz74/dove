f6c8c723d95dc5e0b45dd6fe901d85dd4b8766a0
Leslie Castelan Chavez <lesliecastelanchavez@gmail.com>
2021-11-30 00:27:32 -0800

    basic image upload using method=post

--- react_dove/src/Register.js
+++ react_dove/src/Register.js
@@ -123,6 +123,11 @@ export class Register extends React.Component {
                     </input> <br /><br />
 
 
+                    <h1>Upload Image</h1>
+                    <form action="/register" enctype="multipart/form-data" method="POST">
+                     <input type="file" name="myImage" accept="image/*"/>
+        
+                    </form>
                     {/* <UploadImages></UploadImages> */}
                     <input type="submit" value="Submit" />
                     
