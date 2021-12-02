5313b2685aeaedfd9437cf673a9967ff7993fb85
ryanldoan <69395683+ryanldoan@users.noreply.github.com>
2021-12-02 12:06:31 -0800

    Resolved conflicts before merge

--- react_dove/src/Matches.js
+++ react_dove/src/Matches.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import "./Results.css";
+//import React from 'react';
+import React, { useState } from 'react';
+import "./Matches.css";
 
 async function getMatches(){
     // let matches = [ ]
@@ -11,50 +12,225 @@ async function getMatches(){
     // console.log(matches);
 }
 
-var users = [
-    {
-        name: 'The Rock',
-        age: '49',
-        bio: 'Dwayne Douglas Johnson, also known by his ring name The Rock, is an American actor, producer, businessman, and former professional wrestler. Regarded as one of the greatest professional wrestlers of all time, he wrestled for WWE for eight years prior to pursuing an acting career. ',
-        url: 'https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg'
-    },
-    {
-        name: 'Wesley Snipes',
-        age: '59',
-        bio: 'Wesley Trent Snipes is an American actor, film producer, and martial artist. His prominent film roles include New Jack City, White Men Cant Jump, Passenger 57, Rising Sun, Demolition Man, To Wong Foo, Thanks for Everything!',
-        url: 'https://i.harperapps.com/authors/46999/x500.JPG'
-    }, 
-    {
-        name: 'Zac Efron',
-        age: '34',
-        bio: 'Zachary David Alexander Efron is an American actor and singer. He began acting professionally in the early 2000s and rose to prominence in the late 2000s for his leading role as Troy Bolton in the High School Musical trilogy. During this time, he also starred in the musical film Hairspray and the comedy film 17 Again.',
-        url: 'https://celebrityinsider.org/wp-content/uploads/2020/04/Zac-Efron-HelloGiggles.com_-e1587139680146.jpg'
-    }
-];
-
 function NewMatches () {
     console.log(getMatches())
 
     var matches = getMatches();
+    console.log("hello from NewMatches");
     console.log(getMatches());
+
+    var list_matches = [ ];
+    const [ numMatches, setNumMatches ] = useState("");
+    matches.then(function(match){
+        setNumMatches(match.length);
+    })
+    const [ firstNameS, setFirstName] = useState("");
+    const [ lastNameS, setLastName] = useState("");
+    const [ bioS, setBio ] = useState("");
+    const [ ageS, setAge] = useState("");
+    const [ urlS, setUrl] = useState("");
+
+    const [ firstNameS2, setFirstName2] = useState("");
+    const [ lastNameS2, setLastName2] = useState("");
+    const [ bioS2, setBio2 ] = useState("");
+    const [ ageS2, setAge2 ] = useState("");
+    const [ urlS2, setUrl2 ] = useState("");
+
+    const [ firstNameS3, setFirstName3] = useState("");
+    const [ lastNameS3, setLastName3] = useState("");
+    const [ bioS3, setBio3 ] = useState("");
+    const [ ageS3, setAge3 ] = useState("");
+    const [ urlS3, setUrl3 ] = useState("");
+
+    var numLeft = numMatches;
+    {/*const [ firstNameS4, setFirstName4] = useState("");
+    const [ lastNameS4, setLastName4] = useState("");
+    const [ bioS4, setBio4 ] = useState("");
+    const [ ageS4, setAge4 ] = useState("");
+const [ urlS4, setUrl4 ] = useState("");*/}
+
+    matches.then(function(user){
+        if(numLeft == 0){
+            setFirstName('Add More Matches!');
+            setLastName('');
+            setBio('');
+            setAge('');
+            //setUrl("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+            setUrl('./userprofile.jpg')
+
+            setFirstName2('Add More Matches!');
+            setLastName2('');
+            setBio2('');
+            setAge2('');
+            //setUrl2("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+            setUrl2('./userprofile.jpg');
+
+            setFirstName3('Add More Matches!');
+            setLastName3('');
+            setBio3('');
+            setAge3('');
+            //setUrl3("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+            setUrl3('./userprofile.jpg');
+            return;
+        }
+        var idx = 1
+        setFirstName(user[idx].firstName);
+        setLastName(user[idx].lastName);
+        setBio(user[idx].bio);
+        setAge(user[idx].age);
+        //setUrl("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+        setUrl(user[idx].profile_pic)
+        numLeft = numLeft - 1;
+        if(numLeft == 0)
+        {
+            setFirstName2('Add More Matches!');
+            setLastName2('');
+            setBio2('');
+            setAge2('');
+            //setUrl2("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+            setUrl2('./userprofile.jpg');
+
+            setFirstName3('Add More Matches!');
+            setLastName3('');
+            setBio3('');
+            setAge3('');
+            //setUrl3("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+            setUrl3('./userprofile.jpg');
+            return;
+        }
+        var idx2 = 2
+        setFirstName2(user[idx2].firstName);
+        setLastName2(user[idx2].lastName);
+        setBio2(user[idx2].bio);
+        setAge2(user[idx2].age);
+        //setUrl2("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+        setUrl2(user[idx2].profile_pic);
+        
+        numLeft = numLeft - 1;
+        if(numLeft == 0)
+        {
+            setFirstName3('Add More Matches!');
+            setLastName3('');
+            setBio3('');
+            setAge3('');
+            //setUrl3("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+            setUrl3('./userprofile.jpg');
+            return;
+        }
+        var idx3 = 3
+        setFirstName3(user[idx3].firstName);
+        setLastName3(user[idx3].lastName);
+        setBio3(user[idx3].bio);
+        setAge3(user[idx3].age);
+        //setUrl3("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+        setUrl3(user[idx3].profile_pic);
+
+        {/*var idx4 = 4
+        setFirstName4(user[idx4].firstName);
+        setLastName4(user[idx4].lastName);
+        setBio4(user[idx4].bio);
+        setAge4(user[idx4].age);
+        //setUrl4("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+    setUrl4(user[idx4].profile_pic);*/}
+    })
+
+    {/*var CurrUser1 = {
+        name: firstNameS,
+        age: ageS,
+        bio: bioS,
+        url: urlS
+    }
+
+    var CurrUser2 = {
+        name: firstNameS2,
+        age: ageS2,
+        bio: bioS2,
+        url: urlS2
+    }
+
+    var CurrUser3 = {
+        name: firstNameS3,
+        age: ageS3,
+        bio: bioS3,
+        url: urlS3
+    }
+
+    var CurrUser4 = {
+        name: firstNameS4,
+        age: ageS4,
+        bio: bioS4,
+        url: urlS4
+    }
+
+    users.push(CurrUser1)
+    users.push(CurrUser2)
+    users.push(CurrUser3)
+users.push(CurrUser4)*/}
 
     return(
         // BEM
+        /*<div className="newmatches">
+            <form method = "POST" action = "/matches">
+
+
+            <input type="submit" value = "Show Matches" />
+            </form>
+        </div>*/
         <div className="newmatches">
-            <div>
-                <table className="results">
-                    <tbody>
-                        {users && users.map(user =>
-                            <tr key={user.name}>
-                                <td className="name">{user.name}</td>
-                                <td className="bio">{user.age}, {user.bio}</td>
-                                <hr color="white"></hr>
-                            </tr>
-                        )}
-                    </tbody>
-                </table>
+            <h1>Top 3 Matches</h1>
+            <div class = "image-container">
+                <img src={urlS} alt='profile pic'/>
             </div>
+            <h3>{firstNameS}</h3>
+            <h5>{ageS}</h5>
+            <p>{bioS}</p>
+
+            <br></br>
+            <div class = "image-container">
+                <img src={urlS2} alt='profile pic'/>
+            </div>
+            <h3>{firstNameS2}</h3>
+            <h5>{ageS2}</h5>
+            <p>{bioS2}</p>
+
+            <br></br>
+            <div class = "image-container">
+                <img src={urlS3} alt='profile pic'/>
+            </div>
+            <h3>{firstNameS3}</h3>
+            <h5>{ageS3}</h5>
+            <p>{bioS3}</p>
+
+            {/*<div className= "card-container" >
+            <div class= 'upper-container'> 
+                <div class= "image-container">
+                    {/*<img src={profilepic} alt="profile pic"/>*/}
+                    {/*<img src='./userprofile.jpg' alt='profile pic'/>
+                </div>
+            </div>
+            <div class = 'lower-container'>
+                <div>
+                    {/*<h3 class = 'firstname'>{firstname}</h3>
+                    <h3 class = 'lastname'>{lastname}</h3> 
+                    <h3 class = 'agetodisplay'>{Age}</h3>
+                    <p className= 'emailStyle'>{Email}</p>*/}
+                    {/*<h3 class = 'firstname'></h3>
+                    <h3 class = 'lastname'></h3> 
+                    <h3 class = 'agetodisplay'></h3>
+                    <p className= 'emailStyle'></p>
+                    
+                </div>
+                <br></br>
+                <div>
+                    <p>  </p>
+                </div>
+            </div>
+
+
+
+        </div>*/}
         </div>
+        
     )
 }
 export default NewMatches
--- react_dove/src/Suggested.js
+++ react_dove/src/Suggested.js
@@ -1,46 +1,171 @@
-import React from 'react';
-import "./Results.css";
+import React, {useState, useMemo, useRef}  from 'react'
+import "./Suggested.css";
 
-
-var users = [
-    {
-        name: 'The Rock',
-        age: '49',
-        bio: 'Dwayne Douglas Johnson, also known by his ring name The Rock, is an American actor, producer, businessman, and former professional wrestler. Regarded as one of the greatest professional wrestlers of all time, he wrestled for WWE for eight years prior to pursuing an acting career. ',
-        url: 'https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg'
-    },
-    {
-        name: 'Wesley Snipes',
-        age: '59',
-        bio: 'Wesley Trent Snipes is an American actor, film producer, and martial artist. His prominent film roles include New Jack City, White Men Cant Jump, Passenger 57, Rising Sun, Demolition Man, To Wong Foo, Thanks for Everything!',
-        url: 'https://i.harperapps.com/authors/46999/x500.JPG'
-    }, 
-    {
-        name: 'Zac Efron',
-        age: '34',
-        bio: 'Zachary David Alexander Efron is an American actor and singer. He began acting professionally in the early 2000s and rose to prominence in the late 2000s for his leading role as Troy Bolton in the High School Musical trilogy. During this time, he also starred in the musical film Hairspray and the comedy film 17 Again.',
-        url: 'https://celebrityinsider.org/wp-content/uploads/2020/04/Zac-Efron-HelloGiggles.com_-e1587139680146.jpg'
-    }
-];
-
-function Suggested() {
-
-    return (
-        <div>
-            <table className="results">
-                <tbody>
-                    {users && users.map(user =>
-                        <tr key={user.name}>
-                            <td className="name">{user.name}</td>
-                            <td className="bio">{user.age}, {user.bio}</td>
-                            <hr color="white"></hr>
-                        </tr>
-                        
-                    )}
-                </tbody>
-            </table>
-        </div>
-    );
+async function getSuggested(){
+    const response = await fetch('/suggested');
+    const data = await response.json();
+    return data;
 }
 
-export default Suggested;
+
+function Suggested () {
+    var suggested_list = getSuggested();
+
+
+    const [ numSuggestions, setNumSuggestions ] = useState("");
+    suggested_list.then(function(suggestion){
+        setNumSuggestions(suggestion.length);
+    })
+    const [ firstNameS, setFirstName] = useState("");
+    const [ lastNameS, setLastName] = useState("");
+    const [ bioS, setBio ] = useState("");
+    const [ ageS, setAge] = useState("");
+    const [ urlS, setUrl] = useState("");
+
+    const [ firstNameS2, setFirstName2] = useState("");
+    const [ lastNameS2, setLastName2] = useState("");
+    const [ bioS2, setBio2 ] = useState("");
+    const [ ageS2, setAge2 ] = useState("");
+    const [ urlS2, setUrl2 ] = useState("");
+
+    const [ firstNameS3, setFirstName3] = useState("");
+    const [ lastNameS3, setLastName3] = useState("");
+    const [ bioS3, setBio3 ] = useState("");
+    const [ ageS3, setAge3 ] = useState("");
+    const [ urlS3, setUrl3 ] = useState("");
+
+    var numLeft = numSuggestions;
+
+    suggested_list.then(function(user){
+        if(numLeft === 0){
+            setFirstName('Add More Matches!');
+            setLastName('');
+            setBio('');
+            setAge('');
+            //setUrl("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+            setUrl('./userprofile.jpg')
+
+            setFirstName2('Add More Matches!');
+            setLastName2('');
+            setBio2('');
+            setAge2('');
+            //setUrl2("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+            setUrl2('./userprofile.jpg');
+
+            setFirstName3('Add More Matches!');
+            setLastName3('');
+            setBio3('');
+            setAge3('');
+            //setUrl3("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+            setUrl3('./userprofile.jpg');
+            return;
+        }
+        var idx = 1
+        setFirstName(user[idx].firstName);
+        setLastName(user[idx].lastName);
+        setBio(user[idx].bio);
+        setAge(user[idx].age);
+        //setUrl("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+        setUrl(user[idx].profile_pic)
+        numLeft = numLeft - 1;
+        if(numLeft === 0)
+        {
+            setFirstName2('Add More Matches!');
+            setLastName2('');
+            setBio2('');
+            setAge2('');
+            //setUrl2("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+            setUrl2('./userprofile.jpg');
+
+            setFirstName3('Add More Matches!');
+            setLastName3('');
+            setBio3('');
+            setAge3('');
+            //setUrl3("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+            setUrl3('./userprofile.jpg');
+            return;
+        }
+        var idx2 = 2
+        setFirstName2(user[idx2].firstName);
+        setLastName2(user[idx2].lastName);
+        setBio2(user[idx2].bio);
+        setAge2(user[idx2].age);
+        //setUrl2("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+        setUrl2(user[idx2].profile_pic);
+        
+        numLeft = numLeft - 1;
+        if(numLeft === 0)
+        {
+            setFirstName3('Add More Matches!');
+            setLastName3('');
+            setBio3('');
+            setAge3('');
+            //setUrl3("https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg");
+            setUrl3('./userprofile.jpg');
+            return;
+        }
+        var idx3 = 3
+        setFirstName3(user[idx3].firstName);
+        setLastName3(user[idx3].lastName);
+        setBio3(user[idx3].bio);
+        setAge3(user[idx3].age);
+        setUrl3(user[idx3].profile_pic);
+    })
+    {/*const [ firstNameS, setFirstName] = useState("");
+    const [ lastNameS, setLastName] = useState("");
+    const [ bioS, setBio ] = useState("");
+
+    const [ firstNameS2, setFirstName2] = useState("");
+    const [ lastNameS2, setLastName2] = useState("");
+    const [ bioS2, setBio2 ] = useState("");
+
+    suggested_list.then(function(list){
+        setFirstName(list[0].firstName);
+        setLastName(list[0].lastName);
+        setBio(list[0].bio);
+
+        setFirstName2(list[1].firstName);
+        setLastName2(list[1].lastName);
+        setBio2(list[1].bio);
+    })
+
+    var suggested1 = {
+        name: firstNameS + ' ' + lastNameS,
+        bio: bioS
+    }
+
+    var suggested2 = {
+        name: firstNameS2 + ' ' + lastNameS2,
+        bio: bioS2
+    }*/}
+
+    return(
+        // BEM
+        <div className="suggested">
+            <h1>Top 3 Suggested</h1>
+            <div class = "image-container">
+                <img src={urlS} alt='profile pic'/>
+            </div>
+            <h3>{firstNameS}</h3>
+            <h5>{ageS}</h5>
+            <p>{bioS}</p>
+
+            <br></br>
+            <div class = "image-container">
+                <img src={urlS2} alt='profile pic'/>
+            </div>
+            <h3>{firstNameS2}</h3>
+            <h5>{ageS2}</h5>
+            <p>{bioS2}</p>
+
+            <br></br>
+            <div class = "image-container">
+                <img src={urlS3} alt='profile pic'/>
+            </div>
+            <h3>{firstNameS3}</h3>
+            <h5>{ageS3}</h5>
+            <p>{bioS3}</p>
+        </div>
+    )
+}
+export default Suggested
